7716c5568297c5a5ffe4455ec721098f0d21c3bd
javiercarpio57 <javierrr.carpio57@gmail.com>
2020-02-01 09:56:53 -0600

    Scripts

---
+++ Breaver/Assets/Scripts/Launch.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Launch : MonoBehaviour
+{
+
+    [SerializeField] private float radio;
+    private Transfom center;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        center = GetComponentInChildren<Transfrom>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
