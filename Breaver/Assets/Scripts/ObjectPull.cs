767adfa7acc807c55c3c060f329cf01e97c77d34
Paul De Jesús Belches Flores-Gómez <paulbelches@gmail.com>
2020-02-01 10:27:56 -0600

    Object pull

---
+++ Breaver/Assets/Scripts/ObjectPull.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fallingObjectPull : MonoBehaviour
+{
+    public GamefallingObject fallingObject;
+    public GamefallingObject poolParent;
+    private Queue<GamefallingObject> pool;
+    private int poolSize = 3;
+
+    void Start()
+    {
+        pool = new Queue<GamefallingObject>();
+        IncreasePool();
+    }
+
+    public void ReturnToPool(GamefallingObject toRecycle) {
+        toRecycle.SetActive(false);
+        toRecycle.gamefallingObject.transform.SetParent(poolParent.transform);
+        pool.Enqueue(toRecycle);
+    }
+
+
+    private void IncreasePool() {
+        
+        for (int i = 0; i < poolSize; i++) {
+            GamefallingObject newfallingObject = Instantiate(fallingObject, poolParent.transform);
+            Food newfallingObject = newfallingObject.GetComponent<Food>();
+            newfallingObject.SetPool(this);
+            newfallingObject.SetActive(false);
+            pool.Enqueue(newfallingObject);
+        }
+    }
+
+    public MovablefallingObject GetfallingObject() {
+        if (pool.Count == 0) {
+            IncreasePool();
+        }
+        GamefallingObject newfallingObject = pool.Dequeue();
+        newfallingObject.SetActive(true);
+        MovablefallingObject movable = newfallingObject.GetComponent<MovablefallingObject>();
+        return movable;
+    }
+}
