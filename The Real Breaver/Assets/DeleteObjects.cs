d1363d1d20cff396ef33091f2d9c86ab0d3b7365
javiercarpio57 <javierrr.carpio57@gmail.com>
2020-02-02 15:18:31 -0600

    hola

---
+++ The Real Breaver/Assets/DeleteObjects.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteObjects : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<Object>() != null)
+            Destroy(other.gameObject);
+    }
+}
--- The Real Breaver/Assets/Scripts/Object.cs
+++ The Real Breaver/Assets/Scripts/Object.cs
@@ -37,6 +37,7 @@ public class Object : MonoBehaviour
 
         if (cantObjetos == 0) CancelInvoke("DoIStop");
     }
+    
 
     public void Unparent()
     {
